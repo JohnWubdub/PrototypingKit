e6097050e67c6cba806e29e785035220d8b459a9
John <jw4429@nyu.edu>
2020-05-23 15:25:10 -0400

    Clean Up

---
+++ Coding Utilities/DestroyAfterTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Gets rid of the attached GameObject after a specified amount of (scaled) time.
+//Could be updated to use with an object pool (SetActive(false) instead of Destroy(gameObject)), or unscaled deltaTime.
+//Hella simple, but I find myself making this script a lot.
+public class DestroyAfterTime : MonoBehaviour
+{
+    public float TimeUntilDestruction;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = TimeUntilDestruction;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
