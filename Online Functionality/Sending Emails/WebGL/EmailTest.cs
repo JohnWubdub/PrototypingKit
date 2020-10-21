5ad9edc5936184ca4004e79f820b752980321687
johnwubdub <jw4429@nyu.edu>
2020-10-21 13:45:40 -0400

    Added Online Functionality Folder Added Emailing Scripts for WebGL(tested), Windows(tested) / Mac(untested) Added a webdings font for funny characters hehehe

---
+++ Online Functionality/Sending Emails/WebGL/EmailTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using UE.Email;
+
+public class EmailTest : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    public void SendMail(){
+        Email.SendEmail("poemcollecting@gmail.com", "poemcollecting@gmail.com", "subject", "body", "smtp.gmail.com", "poemcollecting@gmail.com", "technodramatists");
+        /*
+        MailMessage m = new MailMessage();
+        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+        SmtpServer.Timeout = 10000;
+        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+        SmtpServer.UseDefaultCredentials = false;
+        SmtpServer.UseDefaultCredentials = false;
+        SmtpServer.Port = 587;
+        SmtpServer.EnableSsl = true;
+
+        m.From = new MailAddress("casuallyhardcoremail@gmail.com");
+        m.To.Add(new MailAddress("casuallyhardcoremail@gmail.com"));
+
+        m.Subject = "FUCK YOU IT WORKS!!";
+        m.Body = "fuck yeah";
+
+        SmtpServer.Credentials = new System.Net.NetworkCredential("casuallyhardcoremail@gmail.com","madison1998");
+        ServicePointManager.ServerCertificateValidationCallback = delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors){
+            return true;
+        };
+
+        m.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+        SmtpServer.Send(m);
+        */
+    }
+    
+
+    
+}
---
+++ Online Functionality/Sending Emails/Windows (Mac build not tested yet)/EmailTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+//last edit by John Wanamaker
+public class EmailTest : MonoBehaviour
+{
+    //attach this to a button to run
+    //WARNING!! THIS IS NOT SECURE PLEASE DO NOT USE THIS IN A REAL GAME THAT SOMEONE CAN DATA MINE OR USE A PUBLIC STRING THAT IS ASSIGNED IN THE EDITOR TO HIDE IT WHICH IS PROBABLY NOT SECURE EITHER
+    //make sure the from_emailaddress allows less secure apps to access it
+    //make sure there is an internet connection
+
+    public void SendMail()
+    {
+        MailMessage m = new MailMessage();
+        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+        SmtpServer.Timeout = 10000;
+        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+        SmtpServer.UseDefaultCredentials = false;
+        SmtpServer.UseDefaultCredentials = false;
+        SmtpServer.Port = 587;
+        SmtpServer.EnableSsl = true;
+
+        m.From = new MailAddress("from_emailaddress");
+        m.To.Add(new MailAddress("to_emailaddress"));
+
+        m.Subject = "Subject";
+        m.Body = "Body";
+
+        SmtpServer.Credentials = new System.Net.NetworkCredential("from_emailaddress","password");
+        ServicePointManager.ServerCertificateValidationCallback = delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors){
+            return true;
+        };
+
+        m.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+        SmtpServer.Send(m);
+    }
+    
+
+    
+}
