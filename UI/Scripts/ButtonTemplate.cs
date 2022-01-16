ce24a424715f71d50b2bd0e837018e387efd2cb1
John <jw4429@nyu.edu>
2022-01-16 15:56:00 -0500

    Edited and cleaned up

---
+++ UI/Scripts/ButtonTemplate.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonTemplate : MonoBehaviour 
+    , IPointerClickHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+    , IPointerDownHandler
+    , IPointerUpHandler
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    public void OnPointerClick(PointerEventData eventData) 
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+    
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
+
